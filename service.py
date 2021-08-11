7af014d6c2403b84407ba6af535dd56e810ce9db
Edmilson Prata <edprata@gmail.com>
2021-08-10 23:33:23 -0300

    Ajustes para atribuir a porta automaticamente

--- service.py
+++ service.py
@@ -1,7 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
+app = Flask(__name__, static_folder='static')
 
-app = Flask(__name__)
-
-@app.route("/")
+@app.route('/')
 def hello_world():
-    return "<p>Hello, World!</p>"
+    #return render_template('index.html')
+    return "<b>Hello!</b>"
+
+if __name__ == '__main__':
+    from os import environ
+    app.run(debug=False, host='0.0.0.0', port=environ.get("PORT", 5000))
